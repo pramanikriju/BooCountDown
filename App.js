9b4f540c1dcd89ff243d2cc130e379ce57a43d3e
rijupramanik <pramanikriju@gmail.com>
2019-07-24 12:06:01 +0530

    boilerplate

--- App.js
+++ App.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 import { Container, Button, Text } from 'native-base';
+import moment from 'moment';
+
 
 
 export default class GeneralExample extends Component {
+  constructor(props) {
+    super(props);
+    //initialize the counter duration
+    this.state = {
+      totalDuration: '',
+    };
+  }
+
+  componentDidMount() {
+    var that = this;
+
+    //We are showing the coundown timer for a given expiry date-time
+    //If you are making an quize type app then you need to make a simple timer
+    //which can be done by using the simple like given below
+    //that.setState({ totalDuration: 30 }); //which is 30 sec
+
+    var date = moment()
+      .utcOffset('+05:30')
+      .format('YYYY-MM-DD hh:mm:ss');
+    //Getting the current date-time with required formate and UTC   
+    
+    var expirydate = '2019-10-23 04:00:45';//You can set your own date-time
+    //Let suppose we have to show the countdown for above date-time 
+
+    var diffr = moment.duration(moment(expirydate).diff(moment(date)));
+    //difference of the expiry date-time given and current date-time
+
+    var hours = parseInt(diffr.asHours());
+    var minutes = parseInt(diffr.minutes());
+    var seconds = parseInt(diffr.seconds());
+    
+    var d = hours * 60 * 60 + minutes * 60 + seconds;
+    //converting in seconds
+
+    that.setState({ totalDuration: d });
+    //Settign up the duration of countdown in seconds to re-render
+  }
   render() {
     return (
       <Container>
