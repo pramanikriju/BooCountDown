46f057b8ee0c38d138521aa50a18820b8f739b9b
rijupramanik <pramanikriju@gmail.com>
2019-07-25 16:57:18 +0530

    layout working

--- App.js
+++ App.js
@@ -1,45 +1,84 @@
-import React, { Component } from 'react';
-import { Container, Header, Title, Content, Button, Left, Right, Body, Icon, Text } from 'native-base';
-import moment from 'moment';
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from "native-base";
+import { StyleSheet, View } from "react-native";
+import moment from "moment";
+import axios from 'axios';
 
 
-
-export default class GeneralExample extends Component {
+export default class BooCountDown extends Component {
   constructor(props) {
     super(props);
+    var date = moment()
+      .utcOffset("+05:30")
+      .format("YYYY-MM-DD hh:mm:ss");
 
-    var milliseconds = '';
+    //var expirydate = "2019-08-29 07:00:45"
+    axios.get('https://riju.co/boo.php')
+    .then(response => {
+      this.setState({ expiry: response.data.date });
+      var diffr = moment.duration(moment(response.data.date).diff(moment(date)));
+      var milliseconds = diffr.asMilliseconds();
 
-    this.state = { 
-      days : '',
-      hours : '',
-      minutes : '',
-      seconds: '',
-      milliseconds: '99999999',
-     };
+    })
+    .catch(error => {
+      this.setState({ expiry:  "2019-08-29 07:00:45" });
+      var milliseconds = '99999999';
+    });
+
+
+    //var diffr = moment.duration(moment(expirydate).diff(moment(date)));
+
+   
+
+    this.state = {
+      days: "",
+      hours: "",
+      minutes: "",
+      seconds: "",
+      weeks: "",
+      milliseconds: milliseconds,
+    };
   }
 
   tick() {
     var date = moment()
-      .utcOffset('+05:30')
-      .format('YYYY-MM-DD hh:mm:ss');
+      .utcOffset("+05:30")
+      .format("YYYY-MM-DD hh:mm:ss");
 
-    var expirydate = '2019-08-29 07:00:45';
-    
- 
-    var diffr = moment.duration(moment(expirydate).diff(moment(date)));
+    var expirydate = "2019-08-29 07:00:45";
+
+    var diffr = moment.duration(moment(this.state.expiry).diff(moment(date)));
     //difference of the expiry date-time given and current date-time
     var days = parseInt(diffr.asDays());
     var hours = parseInt(diffr.asHours());
     var minutes = parseInt(diffr.minutes());
     var seconds = parseInt(diffr.seconds());
+    var weeks = parseInt(diffr.asWeeks());
     this.setState(prevState => ({
       days: days,
       hours: hours,
       minutes: minutes,
       seconds: seconds,
-      milliseconds : prevState.milliseconds -1,
+      weeks: weeks,
+      milliseconds: prevState.milliseconds - 1
     }));
+
+    var styles = StyleSheet.create({
+      container: {
+        backgroundColor: "blue"
+      }
+    });
   }
 
   componentDidMount() {
@@ -51,43 +90,43 @@ export default class GeneralExample extends Component {
   }
 
   render() {
-
     return (
-      <Container>
-      <Header>
-        <Left>
-          <Button transparent>
-            <Icon name='clock' />
-          </Button>
-        </Left>
-        <Body>
-          <Title>Boo</Title>
-        </Body>
-        <Right />
-      </Header>
-      <Content>
-        <Text>
-         {this.state.days}
-        </Text>
-        <Text>
-        {this.state.hours}
-        </Text>
-        <Text>
-        {this.state.minutes}
-        </Text>
-        <Text>
-        {this.state.seconds}
-        </Text>
-        <Text>
-        {this.state.milliseconds}
-        </Text>
-        <Text>
-        {this.state.check}
-        </Text>
-      
-      
-      </Content>
-    </Container>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 0.5, backgroundColor: "powderblue" }}>
+          <Body>
+            <Title style={{ margin: 8 }}>
+              <Icon name="clock" style={{}} /> Boo
+            </Title>
+          </Body>
+        </View>
+        <View style={{ flex: 4, flexDirection: "row" }}>
+          <View style={{ flex: 2, flexDirection: "column" }}>
+            <View style={{ flex: 1, backgroundColor: "#9E9E9E" }}>
+              <Text>{this.state.days}</Text>
+            </View>
+            <View style={{ flex: 1, backgroundColor: "#EC407A" }}>
+              <Text>{this.state.days}</Text>
+            </View>
+          </View>
+
+          <View style={{ flex: 2, flexDirection: "column" }}>
+            <View style={{ flex: 2, backgroundColor: "#03A9F4" }}>
+            <Text>{this.state.hours}</Text>
+            </View>
+            <View style={{ flex: 2, backgroundColor: "#8BC34A" }} >
+            <Text>{this.state.minutes}</Text>
+            </View>
+            <View style={{ flex: 2, backgroundColor: "#FF5722" }} >
+            <Text>{this.state.seconds}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={{ flex: 1, backgroundColor: "steelblue" }} >
+        <Text>{this.state.milliseconds}</Text>
+
+        </View>
+      </View>
     );
   }
 }
